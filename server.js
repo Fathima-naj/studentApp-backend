dce4d5be71f42517066edd615eac937bf5a8310d
Fathima-naj <fathimanajamuneer@gmail.com>
2026-02-24 09:35:46 +0530

    fix:error fixed

--- server.js
+++ server.js
@@ -18,6 +18,8 @@ app.use(cors(corOptions))
 app.use(express.json());
 app.use('/api/user',userRouter)
 app.use('/api/student',studentRouter)
-app.listen(5000,()=>{
-    console.log("server running...")
-})
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
