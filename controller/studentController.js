f089b2cdb88bb2a387932b79e89521a9be49a450
Fathima-naj <fathimanajamuneer@gmail.com>
2026-02-24 14:51:40 +0530

    feat:role based access implemented

--- controller/studentController.js
+++ controller/studentController.js
@@ -1,8 +1,42 @@
 import Student from "../model/studentModel.js";
+import User from "../model/userModel.js";
+import bcrypt from "bcryptjs";
 
 export const createStudent = async (req, res) => {
-  const student = await Student.create(req.body);
-  res.status(201).json(student);
+  try {
+    const { name, email, course } = req.body;
+
+    let user = await User.findOne({ email });
+
+    if (!user) {
+      const hashedPassword = await bcrypt.hash("default123", 10);
+
+      user = await User.create({
+        name,
+        email,
+        password: hashedPassword,
+        role: "user"
+      });
+    }
+
+    const existingStudent = await Student.findOne({ userId: user._id });
+    if (existingStudent) {
+      return res.status(400).json({
+        message: "Student already exists"
+      });
+    }
+    const student = await Student.create({
+      name,
+      email,
+      course,
+      userId: user._id
+    });
+
+    res.status(201).json(student);
+
+  } catch (error) {
+    res.status(500).json({ message: error.message});
+  }
 };
 
 export const getStudents = async (req, res) => {
