88ad1fccb9f3d0742844f5a20cfffc6e9c46d1e9
Fathima-naj <fathimanajamuneer@gmail.com>
2026-02-25 15:08:12 +0530

    feat:pagination,filter

---
+++ controller/userController.js
@@ -0,0 +1,54 @@
+import User from "../model/userModel.js";
+
+export const getAllUsers = async (req, res) => {
+  try {
+    const {
+      page = 1,
+      limit = 10,
+      search = "",
+      role,
+      sort = "desc",
+    } = req.query;
+
+    const pageNumber = parseInt(page);
+    const limitNumber = parseInt(limit);
+    const skip = (pageNumber - 1) * limitNumber;
+
+    let filter = {};
+
+    if (search) {
+      filter.$or = [
+        { name: { $regex: search, $options: "i" } },
+        { email: { $regex: search, $options: "i" } },
+      ];
+    }
+
+    
+    if (role) {
+      filter.role = role; 
+    }
+
+    const sortOption = {
+      createdAt: sort === "asc" ? 1 : -1,
+    };
+
+    const totalRecords = await User.countDocuments(filter);
+
+    const users = await User.find(filter)
+      .sort(sortOption)
+      .skip(skip)
+      .limit(limitNumber);
+
+    res.status(200).json({
+      totalRecords,
+      currentPage: pageNumber,
+      totalPages: Math.ceil(totalRecords / limitNumber),
+      users,
+    });
+
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -2,6 +2,7 @@ import express from "express"
 import authMiddleware from "../middleware/authMiddleware.js";
 import roleMiddleware from "../middleware/roleMiddleware.js";
 import { createStudent, deleteStudent, getStudent, getStudents, updateStudent } from "../controller/studentController.js";
+import { getAllUsers } from "../controller/userController.js";
 
 const router=express.Router();
 
@@ -12,5 +13,6 @@ router.get("/getStudent", getStudents);
 router.get("/get/:id", getStudent);
 router.put("/update/:id", updateStudent);
 router.delete("/delete/:id", deleteStudent);
+router.get("/getUser",getAllUsers);
 
 export default router;
